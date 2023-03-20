186d282453a506c899315f354bd24730c6f46c52
Choi seowon <chltjdnjs0718@naver.com>
2023-03-20 21:23:37 +0900

    removed hello.java and added Menu.java

---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("1. 메뉴 전체 보기");
+		System.out.println("2. 메뉴 추가");
+		System.out.println("3. 메뉴 품절");
+		System.out.println("4. 인기메뉴");
+		System.out.println("5. 종료");
+	}
+
+}
