52768b14c2cbb50578d05a0c23d43dc5abc8ce59
Choi seowon <chltjdnjs0718@naver.com>
2023-03-22 22:46:42 +0900

    added menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,14 +1,37 @@
+import java.util.Scanner;
 
 public class Menu {
-
 	public static void main(String[] args) {
-		System.out.println("1. 메뉴 전체 보기");
-		System.out.println("2. 메뉴 추가");
-		System.out.println("3. 메뉴 품절");
-		System.out.println("4. 인기메뉴");
-		System.out.println("5. 종료");
-		System.out.println("5. 종료");
+		int num = 0;
+		Scanner input =  new Scanner(System.in);
 		
+		while(num != 4) {
+			System.out.println("1. 메뉴 전체 보기");
+			System.out.println("2. 메뉴 추가");
+			System.out.println("3. 인기메뉴");
+			System.out.println("4. 종료");
+			System.out.print("1~4번 중에 고르세요:");
+			num = input.nextInt();
+			if (num == 1) {
+				System.out.println("스테이크 플레이트 - 22000Won");
+				System.out.println("투움바 파스타 - 15000Won");
+				System.out.println("양송이 크림 파스타 - 13000Won");
+				System.out.println("오징어 먹물 리조또 - 14000Won");
+				System.out.println("불고기 필라프 - 16000Won");
+				System.out.println("감바스 - 6000Won");
+				System.out.println("샐러드 - 6000Won");
+			}
+			if (num == 2) {
+				System.out.print("메뉴 이름:");
+				String MenuName = input.next();
+				System.out.print("메뉴 가격(Won):");
+				int MenuPrice = input.nextInt();			
+			}
+			if(num == 3) {
+				System.out.println("스테이크 플레이트 - 22000Won");
+				System.out.println("투움바 파스타 - 15000Won");
+				System.out.println("불고기 필라프 - 16000Won");
+			}
+		}
 	}
-
 }
